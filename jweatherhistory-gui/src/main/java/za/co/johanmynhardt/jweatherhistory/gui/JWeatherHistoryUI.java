ce68ee2306cbbbe0242639452c8adfa959fb0a25
Johan Mynhardt <johanmynhardt@gmail.com>
2013-04-28 19:51:32 +0200

    Make use of JGoodies.

--- jweatherhistory-gui/src/main/java/za/co/johanmynhardt/jweatherhistory/gui/JWeatherHistoryUI.java
+++ jweatherhistory-gui/src/main/java/za/co/johanmynhardt/jweatherhistory/gui/JWeatherHistoryUI.java
@@ -1,7 +1,21 @@
 package za.co.johanmynhardt.jweatherhistory.gui;
 
+import javax.swing.*;
+
+import com.jgoodies.looks.plastic.PlasticLookAndFeel;
+import com.jgoodies.looks.plastic.theme.LightGray;
+import com.sun.java.swing.plaf.gtk.GTKLookAndFeel;
+
 public class JWeatherHistoryUI {
 	public static void main(String[] args) {
+		try {
+			PlasticLookAndFeel plasticLookAndFeel = new PlasticLookAndFeel();
+			PlasticLookAndFeel.setCurrentTheme(new LightGray());
+			UIManager.setLookAndFeel(plasticLookAndFeel);// plasticLookAndFeel);
+			//UIManager.setLookAndFeel(new GTKLookAndFeel());
+		} catch (UnsupportedLookAndFeelException e) {
+			e.printStackTrace();
+		}
 		new MainFrame();
 	}
 }
